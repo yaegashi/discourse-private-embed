fefe4d8716856acd81bb3ea8eabade85cb1bc03c
YAEGASHI Takeshi <yaegashi@gmail.com>
2023-07-02 16:41:49 +0000

    Replace TopicEmbed.find_remote

--- lib/private_embed.rb
+++ lib/private_embed.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module PrivateEmbed
-  module EmbedController
+  module EmbedControllerInstanceMethods
     def topics
       if !current_user && SiteSetting.login_required?
         return render 'login_required'
@@ -14,6 +14,28 @@ module PrivateEmbed
         return render 'login_required'
       end
       super
+    end
+  end
+
+  module TopicEmbedClassMethods
+    def find_remote(url)
+      url = UrlHelper.normalized_encode(url)
+      URI.parse(url) # ensure url parses, will raise if not
+      fd = FinalDestination.new(url, validate_uri: true, max_redirects: 5, follow_canonical: true)
+
+      uri = fd.resolve
+      return if uri.blank?
+
+      begin
+        # You have to set up the following hidden settings for this to work
+        # SiteSetting.cache_onebox_response_body
+        # SiteSetting.cache_onebox_response_body_domains
+        html = Oneboxer.fetch_cached_response_body(uri) or uri.read
+      rescue OpenURI::HTTPError, Net::OpenTimeout
+        return
+      end
+
+      parse_html(html, uri.to_s)
     end
   end
 end
--- plugin.rb
+++ plugin.rb
@@ -10,8 +10,12 @@ after_initialize do
   require_relative 'lib/private_embed'
 
   class ::EmbedController
-    prepend PrivateEmbed::EmbedController
+    prepend PrivateEmbed::EmbedControllerInstanceMethods
     skip_before_action :redirect_to_login_if_required
-    append_view_path(File.expand_path('../app/views', __FILE__))
+    append_view_path File.expand_path('../app/views', __FILE__)
+  end
+
+  class ::TopicEmbed
+    singleton_class.prepend PrivateEmbed::TopicEmbedClassMethods
   end
 end
